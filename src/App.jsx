1221fd0c01f36be4949ae38cade921b7b32c474e
ThienNguyen <doithua9669@gmail.com>
2022-03-16 22:01:03 +0700

    setup front-end

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import Home from './pages/home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import './home.scss';
+
+export default function Home() {
+  return (
+    <div className='home'>
+        <Navbar></Navbar>
+    </div>
+  )
+}
