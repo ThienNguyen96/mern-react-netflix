81bb506810df36d1d679e8f323d660c436010204
ThienNguyen <doithua9669@gmail.com>
2022-03-24 22:21:33 +0700

    add View component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import Home from './pages/home/Home';
+import View from './pages/view/View';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      {/* <Home></Home> */}
+      <View />
     </div>
   );
 }
