0d80d90c391d96a7d2b23f1656f1c3f9f03cd124
ThienNguyen <doithua9669@gmail.com>
2022-03-26 15:31:12 +0700

    add login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.scss';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 import View from './pages/view/View';
 
 function App() {
   return (
     <div className="App">
       {/* <Home></Home> */}
-      <View />
+      {/* <View /> */}
+      {/* <Register/> */}
+      <Login/>
     </div>
   );
 }
