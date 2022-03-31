b3a3b3e5b9ee2f9dcc5883690e683f3861280ece
ThienNguyen <doithua9669@gmail.com>
2022-03-31 22:07:59 +0700

    add encrypt for user password

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cryptoJs from 'crypto-js';
 import {UserModel} from '../models/User.js';
 
 
 const router = express.Router();
+
 
 //register
 
@@ -11,12 +13,33 @@ router.post('/register', async(req, res) => {
         const newUser = new UserModel({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: cryptoJs.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
         });
         const user = await newUser.save();
-        res.status(200).json(user);
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
     } catch (error) {
         console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+router.post('/login/', async(req, res) => {
+    try {
+        const user = await UserModel.findOne({username: req.body.username});
+        !user && res.status(401).json('Wrong password or username.');
+
+        const bytes = await cryptoJs.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const validatePassword = bytes.toString(cryptoJs.enc.Utf8) === req.body.password;
+        
+        if (validatePassword) {
+            const {password, ...others} = user._doc;
+            res.status(200).json(others);
+        } else {
+            res.status(401).json('Wrong password or username.');
+        }
+
+    } catch (error) {
         res.status(500).json(error);
     }
 });
