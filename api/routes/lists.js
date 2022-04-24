6713e451366042e1d97f66ab76868d814468da01
ThienNguyen <doithua9669@gmail.com>
2022-04-24 21:13:09 +0700

    update list api

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -21,5 +21,51 @@ router.post('/',verify ,async(req, res) => {
     }
 });
 
+//delete
+
+router.delete('/:id',verify ,async(req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await ListModel.findByIdAndDelete(req.params.id);
+            res.status(200).json('The list has been deleted!');
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json('You are not allowed!');
+    }
+});
+
+//get
+router.get('/', verify, async(req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
+    let list = [];
+
+    try {
+        if(typeQuery) {
+            if(genreQuery) {
+                list = await ListModel.aggregate([
+                    {$sample: {size: 10}},
+                    {$match: {type: typeQuery, genre: genreQuery}}
+                ]);
+            } else {
+                list = await ListModel.aggregate([
+                    {$sample: {size: 10}},
+                    {$match: {type: typeQuery}}
+                ]);
+            }
+        } else {
+            list = await ListModel.aggregate([
+                {$sample: {size: 10}}
+            ]);
+        }
+        res.status(200).json(list);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
 
 export default router;
